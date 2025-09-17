76721eed2bcb07174edcbb525806bfb0ed22c893
Dishant <shoonyam.108@gmail.com>
2025-09-17 22:53:07 +0530

    ViewBAsed Animation completed

--- src/App.jsx
+++ src/App.jsx
@@ -221,6 +221,42 @@ import '../styles/global.css';
 
 // -------end-----------------------
 
+//  ------------ViewBAsed Animation( Doubt )------------
+
+// import React from 'react'
+// import { motion } from 'framer-motion';
+
+// const App = () => {
+//   // const ref = useRe (null);
+//   // const isInView = useInView(ref, { once: true });
+
+//   // useEffect(() => {
+//   //   console.log("Is in view -> ", isInView);
+//   // }, [isInView]);
+
+//   return (
+  
+//       <div style={{ height: '100vh', width: '100vw', backgroundColor: 'white', color: 'black' }}>
+
+//         <div style={{ height: "150vh" }} />
+
+//         <motion.div
+//           style={{ height: "100vh", background: "black" }}
+//           initial={{ opacity: 0 }}
+//           whileInView={{ opacity: 1 }}
+//           transition={{ duration: 1 }}
+//         />
+
+//       </div>
+//   );
+// };
+
+
+// export default App
+
+
+//----------------end--------------------------
+
 
 
 
